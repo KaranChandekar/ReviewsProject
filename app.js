858cc3bbba0ee0af76cfa6d817ac05c74abd88eb
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-11 17:41:28 +0530

    show prev person

--- app.js
+++ app.js
@@ -73,3 +73,12 @@ nextBtn.addEventListener("click", function () {
   }
   showPerson(currentItem);
 });
+
+// show prev person
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
