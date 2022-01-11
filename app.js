93d78ea2343f805ac69de3b980128524051808fd
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-11 17:29:04 +0530

    show person based on item

--- app.js
+++ app.js
@@ -53,9 +53,14 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson (person) { 
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-})
+ };
